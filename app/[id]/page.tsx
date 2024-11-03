c40e1ad5b32bae103f772f6abb346738a593ac2e
Eduardo Cobuci <educobuci@gmail.com>
2024-11-03 20:33:03 -0300

    Reorg folders

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,13 +1,10 @@
 import { Deck } from '@/domain/Deck'
 import { IDeckRepository } from '@/infrastructure/IDeckRepository'
 import { DeckRepository } from '@/infrastructure/DeckRepository'
-import { ModeToggle } from '@/components/ModeToggle'
 import ReactQueryProvider from '@/providers/ReactQueryProvider'
+import DeckEditor from '@/features/deck-editor/DeckEditor'
 
-import Section from './_components/Section'
-import Search from './_components/Search'
-
-export default async function Editor({
+export default async function DeckEditorPage({
   params,
 }: {
   params: Promise<{ id: string }>
@@ -17,40 +14,8 @@ export default async function Editor({
   const deck: Deck = await deckRepository.findById(id)
 
   return (
-    <div className="p-4 space-y-4">
-      <title>{deck.name}</title>
-      <header className="flex w-full">
-        <h1 className="flex-grow scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-          {deck.name}
-        </h1>
-        <div className="flex-shrink">
-          <ModeToggle />
-        </div>
-      </header>
-      <div className="grid grid-cols-[1fr,400px,1fr] gap-4">
-        <ReactQueryProvider>
-          <Section>
-            <Search />
-          </Section>
-          <Section>Details</Section>
-          <Section>
-            <div>
-              <h2>Mainboard</h2>
-              {Array.from(deck.mainboard).map((card) => (
-                <div key={card.name}>{card.name}</div>
-              ))}
-            </div>
-            {deck.sideboard.size > 0 && (
-              <div>
-                <h2>Sideboard</h2>
-                {Array.from(deck.sideboard).map((card) => (
-                  <div key={card.name}>{card.name}</div>
-                ))}
-              </div>
-            )}
-          </Section>
-        </ReactQueryProvider>
-      </div>
-    </div>
+    <ReactQueryProvider>
+      <DeckEditor deck={deck} />
+    </ReactQueryProvider>
   )
 }
---
+++ features/deck-editor/DeckEditor.tsx
@@ -0,0 +1,45 @@
+import { Deck } from '@/domain/Deck'
+import { ModeToggle } from '@/components/ModeToggle'
+import ReactQueryProvider from '@/providers/ReactQueryProvider'
+import Section from '@/features/deck-editor/Section'
+import Search from '@/features/deck-editor/Search'
+
+export default async function DeckEditor({ deck }: { deck: Deck }) {
+  return (
+    <div className="p-4 space-y-4">
+      <title>{deck.name}</title>
+      <header className="flex w-full">
+        <h1 className="flex-grow scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          {deck.name}
+        </h1>
+        <div className="flex-shrink">
+          <ModeToggle />
+        </div>
+      </header>
+      <div className="grid grid-cols-[1fr,400px,1fr] gap-4">
+        <ReactQueryProvider>
+          <Section>
+            <Search />
+          </Section>
+          <Section>Details</Section>
+          <Section>
+            <div>
+              <h2>Mainboard</h2>
+              {Array.from(deck.mainboard).map((card) => (
+                <div key={card.name}>{card.name}</div>
+              ))}
+            </div>
+            {deck.sideboard.size > 0 && (
+              <div>
+                <h2>Sideboard</h2>
+                {Array.from(deck.sideboard).map((card) => (
+                  <div key={card.name}>{card.name}</div>
+                ))}
+              </div>
+            )}
+          </Section>
+        </ReactQueryProvider>
+      </div>
+    </div>
+  )
+}
