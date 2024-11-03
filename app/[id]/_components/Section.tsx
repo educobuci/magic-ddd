19d47700083e394cda16a966c2f2001f98a84b8d
Eduardo Cobuci <educobuci@gmail.com>
2024-11-02 23:45:52 -0300

    Update color

--- app/[id]/_components/Section.tsx
+++ app/[id]/_components/Section.tsx
@@ -1,9 +1,5 @@
 import { ReactNode } from 'react'
 
 export default function Section({ children }: { children: ReactNode }) {
-  return (
-    <section className="rounded-md border border-gray-200 dark:border-gray-800 p-2">
-      {children}
-    </section>
-  )
+  return <section className="rounded-md border p-2">{children}</section>
 }
