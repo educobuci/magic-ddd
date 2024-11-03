046101d08375a627a2d8e1c541f0a54f2e86b6b6
Eduardo Cobuci <educobuci@gmail.com>
2024-11-03 20:43:52 -0300

    Reorg

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 
-import { ThemeProvider } from '@/components/theme-provider'
+import { ThemeProvider } from '@/components/ThemeProvider'
 
 import './globals.css'
 
--- features/deck-editor/Search.tsx
+++ features/deck-editor/Search.tsx
@@ -4,25 +4,25 @@ import { ChangeEvent, useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 
 import { Input } from '@/components/ui/input'
-import { search as doSearch } from '@/services/scryfallService'
+import { search } from '@/services/scryfallService'
 
 export default function Search() {
-  const [search, setSearch] = useState('')
+  const [query, setQuery] = useState('')
 
   const { data: cards } = useQuery({
-    queryKey: ['cards', search],
-    queryFn: () => doSearch(search),
-    enabled: search.length > 2,
+    queryKey: ['cards', query],
+    queryFn: () => search(query),
+    enabled: query.length > 2,
   })
 
   const handleSearchChance = (e: ChangeEvent<HTMLInputElement>): void => {
-    setSearch(e.target.value)
+    setQuery(e.target.value)
   }
 
   return (
     <div className="p-4 space-y-4">
       <Input
-        defaultValue={search}
+        defaultValue={query}
         onChange={handleSearchChance}
         placeholder="Search for cards..."
         autoFocus
